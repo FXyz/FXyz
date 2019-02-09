a6d3c3e16c8a0f8a669718e54005030cec52f562
Almas Baimagambetov <almaslvl@gmail.com>
2019-02-09 19:56:15 +0000

    correctly add maya view to Model3D, refactor MayaImporter, added RunWithFX extension for tests that require active JavaFX, added MayaImporterTest

--- FXyz-Importers/src/main/java/org/fxyz3d/importers/maya/MayaImporter.java
+++ FXyz-Importers/src/main/java/org/fxyz3d/importers/maya/MayaImporter.java
@@ -32,13 +32,6 @@
  */
 package org.fxyz3d.importers.maya;
 
-import javafx.scene.Group;
-import org.fxyz3d.importers.Importer;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.*;
-
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
@@ -46,120 +39,96 @@ import javafx.scene.DepthTest;
 import javafx.scene.Node;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
+import javafx.util.Duration;
+import org.fxyz3d.importers.Importer;
 import org.fxyz3d.importers.Model3D;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+
 /**
- * MayaImporter
- * <p/>
- * MayaImporter.getRoot() returns a JavaFX node hierarchy MayaImporter.getTimeline() returns a JavaFX timeline
+ * Maya Importer with support for Maya "ma" format.
  */
 public class MayaImporter implements Importer {
     public static final boolean DEBUG = Loader.DEBUG;
     public static final boolean WARN = Loader.WARN;
 
-    // NO_JOINTS
-    // javafx.scene.shape3d.Character rootCharacter = new javafx.scene.shape3d.Character();
-    MayaGroup root = new MayaGroup();
-    Timeline timeline;
-
-    // NO_JOINTS
-    // public javafx.scene.shape3d.Character getRootCharacter() {
-    //        return rootCharacter;
-    // }
+    private static final String SUPPORTED_EXT = "ma";
 
     @Override
     public Model3D load(URL url) throws IOException {
-        load(url, false);
-        return new Model3D() {
-            @Override
-            public Optional<Timeline> getTimeline() {
-                return Optional.of(MayaImporter.this.timeline);
-            }
-
-            @Override
-            public Group getRoot() {
-                return MayaImporter.this.root;
-            }
-        };
+        return load(url, false);
     }
 
     @Override
     public Model3D loadAsPoly(URL url) throws IOException {
-        load(url, true);
-        return new Model3D() {
+        return load(url, true);
+    }
+
+    private Model3D load(URL url, boolean asPolygonMesh) {
+        Loader loader = new Loader();
+        loader.load(url, asPolygonMesh);
+
+        // This root is not automatically added to the scene.
+        // It needs to be added by the user of MayaImporter.
+        //            root = new Xform();
+
+        MayaGroup mayaRoot = new MayaGroup();
+        // Add top level nodes to the root
+        int nodeCount = 0;
+        for (Node n : loader.loaded.values()) {
+            if (n != null) {
+                // Only add a node if it has no parents, ie. top level node
+                if (n.getParent() == null) {
+                    if (Loader.DEBUG) {
+                        System.out.println("Adding top level node " + n.getId() + " to root!");
+                    }
+                    n.setDepthTest(DepthTest.ENABLE);
+                    if (!(n instanceof MeshView) || ((TriangleMesh)((MeshView)n).getMesh()).getPoints().size() > 0) {
+                        mayaRoot.getChildren().add(n);
+                    }
+                }
+                nodeCount++;
+            }
+        }
+        // rootCharacter.setRootJoint(loader.rootJoint);
+        if (Loader.DEBUG) {
+            System.out.println("There are " + nodeCount + " nodes.");
+        }
+
+        // if meshes were not loaded in the code above
+        // (which they now are) one would need to
+        // set meshParents from the loader
+        // meshParents.addAll(loader.meshParents.keySet());
+        // this is not necessary at the moment
+
+        Timeline timeline = new Timeline();
+
+        loader.keyFrameMap.forEach((seconds, keyValues) -> {
+            timeline.getKeyFrames().add(
+                    new KeyFrame(Duration.millis(seconds * 1000), keyValues.toArray(new KeyValue[0]))
+            );
+        });
+
+        if (Loader.DEBUG) {
+            System.out.println("Loaded " + timeline.getKeyFrames().size() + " key frames.");
+        }
+
+        Model3D model = new Model3D() {
             @Override
             public Optional<Timeline> getTimeline() {
-                return Optional.of(MayaImporter.this.timeline);
-            }
-
-            @Override
-            public Group getRoot() {
-                return MayaImporter.this.root;
+                return Optional.of(timeline);
             }
         };
+
+        model.addMeshView("default", mayaRoot);
+
+        return model;
     }
 
-    //=========================================================================
-    // MayaImporter.load
-    //=========================================================================
-
-    private void load(URL url, boolean asPolygonMesh) {
-        try {
-            Loader loader = new Loader();
-            loader.load(url, asPolygonMesh);
-
-            // This root is not automatically added to the scene.
-            // It needs to be added by the user of MayaImporter.
-            //            root = new Xform();
-
-            // Add top level nodes to the root
-            int nodeCount = 0;
-            for (Node n : loader.loaded.values()) {
-                if (n != null) {
-                    // Only add a node if it has no parents, ie. top level node
-                    if (n.getParent() == null) {
-                        if (Loader.DEBUG) {
-                            System.out.println("Adding top level node " + n.getId() + " to root!");
-                        }
-                        n.setDepthTest(DepthTest.ENABLE);
-                        if (!(n instanceof MeshView) || ((TriangleMesh)((MeshView)n).getMesh()).getPoints().size() > 0) {
-                            root.getChildren().add(n);
-                        }
-                    }
-                    nodeCount++;
-                }
-            }
-            // rootCharacter.setRootJoint(loader.rootJoint);
-            if (Loader.DEBUG) { System.out.println("There are " + nodeCount + " nodes."); }
-
-            // if meshes were not loaded in the code above
-            // (which they now are) one would need to
-            // set meshParents from the loader
-            // meshParents.addAll(loader.meshParents.keySet());
-            // this is not necessary at the moment
-
-            timeline = new Timeline();
-            int count = 0;
-
-            // Add all the keyframes to the timeline from loader.keyFrameMap
-            for (final Map.Entry<Float, List<KeyValue>> e : loader.keyFrameMap.entrySet()) {
-                // if (DEBUG) System.out.println("key frame at : "+ e.getKey());
-                timeline.getKeyFrames().add
-                        (
-                                new KeyFrame(
-                                        javafx.util.Duration.millis(e.getKey() * 1000f),
-                                        (KeyValue[]) e.getValue().toArray(new KeyValue[e.getValue().size()])));
-                count++;
-            }
-
-            if (Loader.DEBUG) { System.out.println("Loaded " + count + " key frames."); }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
     @Override
     public boolean isSupported(String extension) {
-        return extension != null && extension.equals("ma");
+        return SUPPORTED_EXT.equalsIgnoreCase(extension);
     }
 }
